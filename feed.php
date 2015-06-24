437ea2323221c8417623b42bb64f72e9ddbdedee
Scott Falkingham <scott@falkingham.ca>
2015-06-24 16:09:05 -0500

    Limit title length and remove author name.

--- feed.php
+++ feed.php
@@ -7,8 +7,8 @@ for ($i=0; $i<$arrLen; $i++) {
 	print(PHP_EOL. '	<entry>'. PHP_EOL);
 		print('		<id>tag:facebook.com,' . date("Y-m-d", strtotime($fd[$i]['created_time'])) . ':/' . $fd[$i]['from']['name'] . '/statuses/' . $fd[$i]['id'] . '</id>'. PHP_EOL);
 		print('		<link href="'.$fd[$i]['link'] .'" rel="alternate" type="text/html"/>'. PHP_EOL);
-		print('		<title>'.$fd[$i]['from']['name'].': '.htmlspecialchars($fd[$i]['message']).'</title>'. PHP_EOL);
-		print('		<summary type="html"><![CDATA['.$fd[$i]['from']['name'].': '.$fd[$i]['message'].']]></summary>'. PHP_EOL);
+		print('		<title>'.htmlspecialchars(strlen($fd[$i]['message'])>100 ? substr($fd[$i]['message'],0,50)."..." : $fd[$i]['message']).'</title>'. PHP_EOL);
+		print('		<summary type="html"><![CDATA['.$fd[$i]['message'].']]></summary>'. PHP_EOL);
 
 		$feedContent = '		<content type="html"><![CDATA[<p>'.nl2br($fd[$i]['message']).'</p>]]></content>';
 		$text = processString($feedContent);
